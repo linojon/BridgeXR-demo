4cbdd3a7ce12ce5a067adc0063c2ea19352be0ca
Krystian Babilinski <kbabilinski@gmail.com>
2018-04-11 19:19:09 -0400

    Updated bridge

---
+++ Assets/DelayedEnable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedEnable : MonoBehaviour
+{
+    public GameObject TargetGameObject;
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(5);
+	    TargetGameObject.SetActive(true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
