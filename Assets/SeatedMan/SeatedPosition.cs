62444016bac4520b5b35b575655a101fa640d394
Krystian Babilinski <kbabilinski@gmail.com>
2018-04-17 10:33:43 -0400

    updated seated man

---
+++ Assets/SeatedMan/SeatedPosition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatedPosition : MonoBehaviour
+{
+    public enum Sex
+    {
+        Male,Female
+    }
+
+    public Sex CurrentSex = Sex.Male;
+    public Transform ChairHeightTransform;
+    public Transform EyeLevelTransform;
+
+    void OnValidate()
+    {
+        if(!EyeLevelTransform || !ChairHeightTransform)
+            return;
+        
+        Vector3 chairPosition = ChairHeightTransform.position;
+        switch (CurrentSex)
+        {
+            case Sex.Male:
+                chairPosition.y += .790f;
+                break;
+            case Sex.Female:
+                chairPosition.y += .740f;
+                break;
+        }
+        
+        EyeLevelTransform.position = chairPosition;
+
+    }
+}
