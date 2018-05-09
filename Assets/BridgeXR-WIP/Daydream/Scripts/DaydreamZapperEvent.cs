bcdaf3891f57d733516aa04a42129a13256acd36
Krystian Babilinski <kbabilinski@gmail.com>
2018-05-09 12:33:59 -0400

    Added BridgeXR-WIP folder

---
+++ Assets/BridgeXR-WIP/Daydream/Scripts/DaydreamZapperEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DaydreamElements.ArmModels;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class DaydreamZapperEvent : MonoBehaviour
+{
+    public UnityEvent OnZapped;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetComponent<Projectile>())
+        {
+            OnZapped.Invoke();
+        }
+    }
+}
